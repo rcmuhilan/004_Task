644dd9b396a54a5cd9308375beee21d6b325d3ae
Chandra Muhilan <rcmuhilan@gmail.com>
2026-01-22 18:44:18 +0530

    seperate users file

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,30 +1,23 @@
 import { Injectable} from '@nestjs/common';
 import { AuthPayloadDto } from './dto/auth.dto';
 import { JwtService } from '@nestjs/jwt';
+import { join } from 'path';
+import { readFileSync } from 'fs';
 
-const sampleUsers = [
-    {
-        id: 1,
-        username: 'muhilan',
-        password: '123456'
-    },
-    {
-        id: 2,
-        username: 'chandra',
-        password: '654321'
-    },
-]
 
 @Injectable()
 export class AuthService {
     constructor( private jwtService: JwtService ) {}
     validateUser({ username , password }: AuthPayloadDto){
-        const findUser = sampleUsers.find((user) => user.username === username)
+        const usersPath = join(__dirname, '..', 'data', 'users.json');
+        const sampleUsers = JSON.parse(readFileSync(usersPath, 'utf-8'));
+        const findUser = sampleUsers.users.find((user) => user.username === username)
         if (!findUser) return null;
         if (password === findUser.password) {
             const { password, ...user } = findUser;
             return this.jwtService.sign(user)
         }
+        return null;
     }
 
 }
